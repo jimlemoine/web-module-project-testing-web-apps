70fd6cac3f656bf8941b6d326b593bbba358cc8c
jimlemoine <jlemoine007@gmail.com>
2021-09-30 13:53:55 -0700

    first two problems complete

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -2,3 +2,10 @@
 1. What are some possible tests for this application?
 * The component correctly renders.
 * ...
+The nav and header render correctly
+
+Error messages display correctly
+
+The user can submit the form (we will type in first name, last name, email since they're required)
+
+After submission the user can see what they submitted
--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,19 +5,32 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-    
+    render(<ContactForm />);
 });
 
 test('renders the contact form header', ()=> {
-    
+    render(<ContactForm />)
+    const contactHeader = screen.getByText("Contact Form");
+    expect(contactHeader).toBeInTheDocument();
+    expect(contactHeader).toBeTruthy();
+    expect(contactHeader).toHaveTextContent("Contact Form");
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+    render(<ContactForm />);
+    const firstnameInput = screen.getByLabelText("First Name*");
+    const shortFirstname = "Asd";
+    userEvent.type(firstnameInput, shortFirstname);
+    await waitFor(() => {
+        const firstnameError = screen.queryByText("Error: firstName must have at least 5 characters.");
+        expect(firstnameError).toBeInTheDocument();
+    })
+
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+    render(<ContactForm />);
+    const submitButton = screen
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
